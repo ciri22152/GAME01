dc2e08c2103ed4e51bb5fbe7b1e14a38356ccaf9
kkkkkaito <o.kaito1016@gmail.com>
2025-01-20 02:04:37 +0900

    ドロー操作追加

--- Assets/Script/Battle/FieldManager.cs
+++ Assets/Script/Battle/FieldManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UIイベント用
 
 public class FieldManager : MonoBehaviour
 {
@@ -8,38 +9,77 @@ public class FieldManager : MonoBehaviour
     public Transform trashZone;
     public CardZone playBoardZone; // プレイボードゾーンの参照
 
+    public Button deckButton; // デッキイメージに対応するボタン
+    private List<Transform> remainingCards = new List<Transform>(); // デッキに残っているカードのリスト
+
     public void InitHands(BattleManager _battleManager)
     {
         battleManager = _battleManager;
         Debug.Log("FieldManager.cs : 初期化完了");
 
+        InitializeDeck();
         PlaceRandomCards();
+
+        // デッキイメージのボタンにクリックイベントを登録
+        deckButton.onClick.AddListener(DrawCardFromDeck);
     }
 
-    private void PlaceRandomCards()
+    private void InitializeDeck()
     {
-        List<Transform> cards = new List<Transform>();
-
+        // デッキにカードを登録
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform child = transform.GetChild(i);
             if (child.name.StartsWith("Card"))
             {
-                cards.Add(child);
+                remainingCards.Add(child);
             }
         }
 
-        for (int i = 0; i < cards.Count; i++)
+        // カードをランダムにシャッフル
+        for (int i = 0; i < remainingCards.Count; i++)
         {
-            Transform temp = cards[i];
-            int randomIndex = Random.Range(i, cards.Count);
-            cards[i] = cards[randomIndex];
-            cards[randomIndex] = temp;
+            Transform temp = remainingCards[i];
+            int randomIndex = Random.Range(i, remainingCards.Count);
+            remainingCards[i] = remainingCards[randomIndex];
+            remainingCards[randomIndex] = temp;
         }
+    }
 
-        for (int i = 0; i < 5 && i < cards.Count; i++)
+    private void PlaceRandomCards()
+    {
+        // 初期手札を5枚配置
+        for (int i = 0; i < 5 && remainingCards.Count > 0; i++)
         {
-            cards[i].SetParent(handsCanvas, false);
+            // 先頭のカードを Hands に移動
+            Transform card = remainingCards[0];
+            card.SetParent(handsCanvas, false);
+
+            // デッキから削除
+            remainingCards.RemoveAt(0);
+        }
+    }
+
+
+    private void DrawCardFromDeck()
+    {
+        if (remainingCards.Count > 0)
+        {
+            // 残りのカードからランダムに1枚取得
+            int randomIndex = Random.Range(0, remainingCards.Count);
+            Transform selectedCard = remainingCards[randomIndex];
+
+            // Handsに移動
+            selectedCard.SetParent(handsCanvas, false);
+
+            // デッキから削除
+            remainingCards.RemoveAt(randomIndex);
+
+            Debug.Log($"Card {selectedCard.name} has been drawn and moved to Hands.");
+        }
+        else
+        {
+            Debug.Log("No cards left in the deck.");
         }
     }
 
